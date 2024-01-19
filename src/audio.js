66f769241f4d18d3e6cb9fc8e8b138b8fecf7dab
hobmartin <ivanderda1999@gmail.com>
2024-01-20 00:32:12 +0200

    fix: sounds

--- src/audio.js
+++ src/audio.js
@@ -1,26 +1,32 @@
 const sounds = {
-  newGame: "newgame",
-  boom1: "boom1",
-  boom2: "boom2",
-  boom3: "boom3",
-  boom4: "boom4",
-  slide: "slide",
-  win: "win",
-  lose: "lose",
-  scream: "scream",
+  newGame: "newgame.mp3",
+  boom1: "boom1.mp3",
+  boom2: "boom2.mp3",
+  boom3: "boom3.mp3",
+  boom4: "boom4.mp3",
+  slide: "slide.mp3",
+  win: "win.mp3",
+  lose: "lose.mp3",
+  scream: "scream.mp3",
+};
+
+const buildAudio = (name) => {
+  const source = new URL(`../assets/sounds/${name}`, import.meta.url).href;
+  const audio = new Audio(source);
+  return audio;
 };
 
 export class AudioControl {
   constructor() {
-    this.newGame = document.getElementById(sounds.newGame);
-    this.boom1 = document.getElementById(sounds.boom1);
-    this.boom2 = document.getElementById(sounds.boom2);
-    this.boom3 = document.getElementById(sounds.boom3);
-    this.boom4 = document.getElementById(sounds.boom4);
-    this.slide = document.getElementById(sounds.slide);
-    this.win = document.getElementById(sounds.win);
-    this.lose = document.getElementById(sounds.lose);
-    this.scream = document.getElementById(sounds.scream);
+    this.newGame = buildAudio(sounds.newGame);
+    this.boom1 = buildAudio(sounds.boom1);
+    this.boom2 = buildAudio(sounds.boom2);
+    this.boom3 = buildAudio(sounds.boom3);
+    this.boom4 = buildAudio(sounds.boom4);
+    this.slide = buildAudio(sounds.slide);
+    this.win = buildAudio(sounds.win);
+    this.lose = buildAudio(sounds.lose);
+    this.scream = buildAudio(sounds.scream);
 
     this.boomSounds = [this.boom1, this.boom2, this.boom3, this.boom4];
   }
