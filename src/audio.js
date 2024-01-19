1b7dc55e0ec96ee5a049304a249db4ef2744d415
hobmartin <ivanderda1999@gmail.com>
2024-01-20 00:25:25 +0200

    fix: sounds

--- src/audio.js
+++ src/audio.js
@@ -1,27 +1,26 @@
 const sounds = {
-  newGame: "newgame.mp3",
-  boom1: "boom1.mp3",
-  boom2: "boom2.mp3",
-  boom3: "boom3.mp3",
-  boom4: "boom4.mp3",
-  slide: "slide.mp3",
-  win: "win.mp3",
-  lose: "lose.mp3",
-  scream: "scream.mp3",
+  newGame: "newgame",
+  boom1: "boom1",
+  boom2: "boom2",
+  boom3: "boom3",
+  boom4: "boom4",
+  slide: "slide",
+  win: "win",
+  lose: "lose",
+  scream: "scream",
 };
-const buildSoundSrc = (sound) => `${import.meta.url}/sounds/${sound}`;
 
 export class AudioControl {
   constructor() {
-    this.newGame = new Audio(buildSoundSrc(sounds.newGame));
-    this.boom1 = new Audio(buildSoundSrc(sounds.boom1));
-    this.boom2 = new Audio(buildSoundSrc(sounds.boom2));
-    this.boom3 = new Audio(buildSoundSrc(sounds.boom3));
-    this.boom4 = new Audio(buildSoundSrc(sounds.boom4));
-    this.slide = new Audio(buildSoundSrc(sounds.slide));
-    this.win = new Audio(buildSoundSrc(sounds.win));
-    this.lose = new Audio(buildSoundSrc(sounds.lose));
-    this.scream = new Audio(buildSoundSrc(sounds.scream));
+    this.newGame = document.getElementById(sounds.newGame);
+    this.boom1 = document.getElementById(sounds.boom1);
+    this.boom2 = document.getElementById(sounds.boom2);
+    this.boom3 = document.getElementById(sounds.boom3);
+    this.boom4 = document.getElementById(sounds.boom4);
+    this.slide = document.getElementById(sounds.slide);
+    this.win = document.getElementById(sounds.win);
+    this.lose = document.getElementById(sounds.lose);
+    this.scream = document.getElementById(sounds.scream);
 
     this.boomSounds = [this.boom1, this.boom2, this.boom3, this.boom4];
   }
